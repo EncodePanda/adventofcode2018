79b5227309a722816a41ece451d0519a212d2d40
Pawel Szulc <paul.szulc@gmail.com>
2018-12-01 15:09:19 +0100

    Refactor Day 1, Part 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,15 @@ module Main where
 import Day1
 
 main :: IO ()
-main = part1Program "src/day1part1.input"
+main = part1Program "src/day1part1.input" >>= (putStrLn.show)
 
-part1Program :: FilePath -> IO ()
-part1Program path = (part1 <$> (readFile path)) >>= (putStrLn.show)
+part1Program :: FilePath -> IO Int
+part1Program = (fmap part1).fetchInput
+
+fetchInput :: FilePath -> IO [Int]
+fetchInput path = do
+  content <- readFile path
+  return $ fmap line2Int (lines content)
+    where
+      line2Int ('+':v) = read v
+      line2Int v = read v
--- src/Day1.hs
+++ src/Day1.hs
@@ -2,8 +2,5 @@ module Day1 where
 
 type Input = String
 
-part1 :: Input -> Int
-part1 = sum.(fmap line2Int).lines
-  where
-    line2Int ('+':v) = read v
-    line2Int v = read v
+part1 :: [Int] -> Int
+part1 = sum
