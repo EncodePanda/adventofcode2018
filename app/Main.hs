89d51221b3bffd9412441ec0b7a0e44c7a456691
Pawel Szulc <paul.szulc@gmail.com>
2018-12-04 10:29:07 +0100

    Refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,12 @@ import qualified Day2 as D2
 import qualified Day3 as D3
 
 main :: IO ()
-main = day3Part1Program "src/day3.input" >>= (putStrLn.show)
-
-day1Part1Program :: FilePath -> IO Int
-day1Part1Program = (fmap D1.part1).fetchInput
-
-day1Part2Program :: FilePath -> IO Int
-day1Part2Program = (fmap $ D1.part2).fetchInput
-
-day2Part1Program :: FilePath -> IO Int
-day2Part1Program path = (D2.part1.lines) <$> readFile path
-
-day2Part2Program :: FilePath -> IO String
-day2Part2Program path = ((D2.part2).lines) <$> readFile path
-
-day3Part1Program :: FilePath -> IO Int
-day3Part1Program path = ((D3.part1 999 999).lines) <$> readFile path
+main = do
+  -- ((fmap D1.part1).fetchInput) "src/day1.input" >>= (putStrLn.show)
+  -- ((fmap D1.part2).fetchInput) "src/day1.input" >>= (putStrLn.show)
+  -- (D2.part1.lines) <$> readFile "src/day2.input" >>= (putStrLn.show)
+  -- (D2.part2.lines) <$> readFile "src/day2.input" >>= (putStrLn.show)
+  ((D3.part1 999 999).lines) <$> readFile "src/day2.input" >>= (putStrLn.show)
 
 fetchInput :: FilePath -> IO [Int]
 fetchInput path = do
